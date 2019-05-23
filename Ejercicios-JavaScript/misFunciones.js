fd47273a48552c730566141bb66ef6d54652b61f
EmilianoLuna <1614063@ucc.edu.ar>
2019-05-23 09:49:22 -0300

    EjClases22-05

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,3 +9,22 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function saludar(){
+ alert('hola mundo');
+}
+
+function convertir(id_elem, valor_elem) {
+
+    if(id_elem=='metro'){
+        document.getElementById("pulgada").value = valor_elem * 39.3701;
+    }
+
+}
+
+function convertir(id_elem, valor_elem) {
+    if(id_elem=='metro'){
+        document.getElementById("pie").value = valor_elem * 3.28084;
+    }
+
+}
